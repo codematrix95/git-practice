010a501da1b3b6db378de20564fe94fd718c9aaf
codematrix95 <codematrix95@gmail.com>
2023-08-17 14:05:15 -0500

    more practice

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 console.log("Hey bram just practicing git! Thank you for teaching me!");
 console.log("slowly but surely learning");
+console.log("hey bram just doing some more practice so I don't forget")
